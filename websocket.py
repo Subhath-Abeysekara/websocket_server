e1d67cae3d1c74d4c0ccf79e364c97d9baf7a544
subhath abeysekara <subhath@liveroom.dev>
2023-10-06 01:35:54 +0530

    send first

--- websocket.py
+++ websocket.py
@@ -24,6 +24,7 @@ async def handle_client(websocket, path):
                 score = pq.get()
                 print(score)
                 await websocket.send(str(score))
+                time.sleep(20)
                 # response = await websocket.recv()
                 # print(response)
     except websockets.exceptions.ConnectionClosedOK:
