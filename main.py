f85d7116a3fecf2057da4c51ec909c968eff2678
subhath abeysekara <subhath@liveroom.dev>
2023-10-05 22:53:08 +0530

    change socket

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ async def handle_client(websocket, path):
         while True:
             message = await websocket.recv()
             print(f"Received message: {message}")
-            await websocket.send(f"Received your message: {message}")
+            user_input = input("Enter Reply: ")
+            await websocket.send(f"Received your message: {user_input}")
     except websockets.exceptions.ConnectionClosedOK:
         print("Client disconnected")
 print("server")
-start_server = websockets.serve(handle_client, "ec2-13-234-48-112.ap-south-1.compute.amazonaws.com", 8765)
+start_server = websockets.serve(handle_client, "ec2-13-235-51-177.ap-south-1.compute.amazonaws.com", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
